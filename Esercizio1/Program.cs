88f1b2b24da53cd49c3b318a3bf1ab3191043b96
Olivelli Putelli <olivelliputelli@live.it>
2017-09-20 12:19:38 +0200

    Esercizio 1

---
+++ Esercizio1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Esercizio1
+{
+    class Program
+    {
+        static void Main()
+        {
+            //variabili e inizailizzazione
+            double raggio = 0.0d;
+            double area = 0.0d;
+
+            //input
+            Console.Write("Raggio: ");
+            raggio = double.Parse(Console.ReadLine());
+
+            //elaborazione
+            area = Math.PI * Math.Pow(raggio, 2);
+
+            //output
+            Console.WriteLine($"Il cerchio con raggio {raggio} ha l'area di {area}");
+        }
+    }
+}
