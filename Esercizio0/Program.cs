2e62695e4e1be8993dfe6d4b2152af3a81563d9a
Olivelli Putelli <olivelliputelli@live.it>
2017-09-20 12:09:59 +0200

    Esercizio 0

--- Esercizio0/Program.cs
+++ Esercizio0/Program.cs
@@ -10,8 +10,25 @@ namespace Esercizio0
         static void Main()
         {
             Console.WriteLine("*** TRIANGOLO ISOSCELE ***");
-            Console.WriteLine("");
 
+            //variabili e inizializzazione
+            double b = 0.0d, h = 0.0d;
+            double area = 0.0d, perimetro = 0.0d;
+            double cateto = 0.0d;
+
+            //input
+            Console.Write("Base: ");
+            b = double.Parse(Console.ReadLine());
+            Console.Write("Altezza: ");
+            h = double.Parse(Console.ReadLine());
+
+            // Elaborazione
+            cateto = Math.Sqrt(Math.Pow(b / 2, 2) + Math.Pow(h, 2));
+            area = b * h / 2;
+            perimetro = b + cateto * 2;
+
+            // Output
+            Console.WriteLine($"Il triangolo con base {b} e altezza {h} ha l'area di {area} e il perimetro di {perimetro}");
         }
     }
 }
